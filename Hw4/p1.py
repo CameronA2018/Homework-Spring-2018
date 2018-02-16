915804e77cfc0bed778a223c190a685411cf54f5
CameronA2018 <acameron@sjnma.org>
2018-02-15 22:09:29 -0600

    did the homework

---
+++ Hw4/p1.py
@@ -0,0 +1,6 @@
+s = raw_input('Please write a simple sentence: ')
+
+words = s.split()
+sentence = "\n".join(words)
+
+print (sentence)
---
+++ Hw4/p3.py
@@ -0,0 +1,7 @@
+import random
+
+x = int(raw_input('Please supply a number: '))
+y = int(raw_input('Please supply a second number: '))
+
+z = random.randint(x,y)
+print (z)
